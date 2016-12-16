506dcaa36a493011cda3baeeeeabe7fbdc2de007
kuznetsovandrey76 <and.rey.q@yandex.ru>
2016-12-16 19:59:06 +0300

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+**I am studying**
+- English 
+- Deutsch
---
+++ script.js
@@ -0,0 +1,23 @@
+
+			// Russian, English, Deutsch
+var words = [['я', 'I', 'ich'],
+			 ['ты', 'you', 'du'],
+			 ['он', 'he', 'er'],
+			 ['она', 'she', 'sie'],
+			 ['оно', 'it', 'es'],
+			 ['мы', 'we', 'wir'],
+			 ['вы', 'you', 'ihr'],
+			 ['они', 'they', 'sie']
+			 // You can add here a word and its translation
+			];
+
+$(document).ready(function randomize() {
+	var i = Math.floor(Math.random() * words.length); //Random number
+	$("#ru-word").append(words[i][0]);
+	$("#en-word").append(words[i][1]);
+	$("#de-word").append(words[i][2]);
+});
+
+$('#refresh').click(function(){
+	location.reload();
+});
